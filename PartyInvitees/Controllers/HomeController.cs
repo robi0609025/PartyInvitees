b35aeff8f61fbe418b1cc44f243e426d37241c49
robi0609025 <robi.kuet@gmail.com>
2019-06-12 15:41:22 +1000

    Last commit
    Pro ASP.NET chapter 2
    Mod
    -> Panel class is replaced by card class in RSVP form view.
    Panel is an old bootstrap command which was in version 4
    Thanks

--- PartyInvitees/Controllers/HomeController.cs
+++ PartyInvitees/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvitees.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,10 +12,33 @@ namespace PartyInvitees.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var hour = DateTime.Now.Hour;
+            int hour = DateTime.Now.Hour;
             ViewBag.Greetings = hour < 12 ? "Good Morning" : "Good Afternoon";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm( GuestResponse guestResponse )
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organiser
+                return View("Thanks", guestResponse);
+            }
+            else   
+            {
+                //There is a validation error
+                return View();
+            }
+
+            //return View("Thanks", guestResponse);
         }
     }
 }
